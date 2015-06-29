fd865a96d793eee901597a877c8d7e1945208923
Peter Tripp <peter@chartio.com>
2015-06-29 12:24:11 -0700

    Add upstream debian patches.

---
+++ debian/socks.h
@@ -0,0 +1,22 @@
+/* renamed functions for SOCKS wrappers */
+
+#define connect Rconnect
+#define bind Rbind
+#define getsockname Rgetsockname
+#define getpeername Rgetpeername
+#define accept Raccept
+#define rresvport Rrresvport
+#define bindresvport Rbindresvport
+#define gethostbyname Rgethostbyname
+#define gethostbyname2 Rgethostbyname2
+#define sendto Rsendto
+#define recvfrom Rrecvfrom
+#define recvfrom Rrecvfrom
+#define write Rwrite
+#define writev Rwritev
+#define send Rsend
+#define sendmsg Rsendmsg
+#define read Rread
+#define readv Rreadv
+#define recv Rrecv
+#define recvmsg Rrecvmsg
